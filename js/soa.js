cad2049151212bc6f912a5f9ee1fab84d21ee1b5
YXlukarov <lj991114@163.com>
2019-12-08 20:15:13 +0800

    新闻完成

--- js/soa.js
+++ js/soa.js
@@ -54,3 +54,36 @@ $("#searchPic").on('change',function(){
         alert("上传失败!")
     })
 })
+
+$(".latest_news").ready(function(){
+    var url = 'https://newsapi.org/v2/everything?q=health&' +
+            'language=zh&' + 'pageSize=8&' +
+            'sortBy=relevancy&' +
+            'apiKey=c621ac94609147a09152c13cc8c555f9';
+    var req = new Request(url);
+    fetch(req).then(function(response) {
+            response.json().then(function(data){
+                var newsList = data;
+                //
+                $("img.news1").attr("src",newsList.articles[1].urlToImage);
+                $("h3.news1").text(newsList.articles[1].title);
+                $("p.news1").text(newsList.articles[1].description);
+                $("a.news1").attr("href",newsList.articles[1].url);
+                //
+                $("img.news2").attr("src",newsList.articles[4].urlToImage);
+                $("h3.news2").text(newsList.articles[4].title);
+                $("p.news2").text(newsList.articles[4].description);
+                $("a.news2").attr("href",newsList.articles[4].url);
+                //
+                $("a.news3").attr("href",newsList.articles[2].url);
+                $("a.news3").text(newsList.articles[2].title);
+                //
+                $("a.news4").attr("href",newsList.articles[3].url);
+                $("a.news4").text(newsList.articles[3].title);
+                //
+                $("a.news5").attr("href",newsList.articles[6].url);
+                $("a.news5").text(newsList.articles[6].title);
+            })
+        })
+    
+})
