f4f1244caa7f91f6a80f5a73c5456993e07ffa13
YXlukarov <lj991114@163.com>
2019-12-15 10:58:35 +0800

    实现搜索

--- js/recipe.js
+++ js/recipe.js
@@ -1,45 +1,133 @@
 var url = "http://localhost:8011/recipe/";
-$(".blog_left_sidebar_sign").ready(function(){
-    url = url + "2";
-    $.ajax({
-        type: "GET",
-        url: url
-    }).success(function(msg){
-        var dishList = msg.result.list;
-        //dish1
-        $("#d1image").attr("src",dishList[0].pic);
-        $("#d1name").text(dishList[0].name);
-        $("#d1tag").text(dishList[0].tag);
-        $("#d1content").html(dishList[0].content);
-        $("#d1id").text(dishList[0].id);
-        //dish2
-        $("#d2image").attr("src",dishList[1].pic);
-        $("#d2name").text(dishList[1].name);
-        $("#d2tag").text(dishList[1].tag);
-        $("#d2content").html(dishList[1].content);
-        $("#d2id").text(dishList[1].id);
-        //dish3
-        $("#d3image").attr("src",dishList[2].pic);
-        $("#d3name").text(dishList[2].name);
-        $("#d3tag").text(dishList[2].tag);
-        $("#d3content").html(dishList[2].content);
-        $("#d3id").text(dishList[2].id);
-        //dish4
-        $("#d4image").attr("src",dishList[3].pic);
-        $("#d4name").text(dishList[3].name);
-        $("#d4tag").text(dishList[3].tag);
-        $("#d4content").html(dishList[3].content);
-        $("#d4id").text(dishList[3].id);
-        //dish5
-        $("#d5image").attr("src",dishList[4].pic);
-        $("#d5name").text(dishList[4].name);
-        $("#d5tag").text(dishList[4].tag);
-        $("#d5content").html(dishList[4].content);
-        $("#d5id").text(dishList[4].id);
+var str=decodeURI(location.href);
+var num=str.indexOf("?") 
+str=str.substr(num+1); 
+var value;
+var keyList = new Array;
+var arr=str.split("&"); //各个参数放到数组里
+for(var i=0;i < arr.length;i++){ 
+    num=arr[i].indexOf("="); 
+    if(num>0){ 
+        value=arr[i].substr(num+1);
+        keyList.push(value);
+    }
+}
+console.log(keyList);
 
-    }).fail(function(msg){
-        console.error("error!")
-    })
+var recipe_name = keyList[0];
+var recipe_id = 1;
+if(recipe_name == "减肥"){
+    recipe_id = 2;
+}
+if(recipe_name == "瘦身"){
+    recipe_id = 3;
+}
+if(recipe_name == "消脂"){
+    recipe_id = 4;
+}
+if(recipe_name == "丰胸"){
+    recipe_id = 5;
+}
+if(recipe_name == "美容"){
+    recipe_id = 6;
+}
+if(recipe_name == "养颜"){
+    recipe_id = 7;
+}
+if(recipe_name == "美白"){
+    recipe_id = 8;
+}
+console.log("recipe_id "+recipe_id);
+var isSearch = keyList[1];
+$(".blog_left_sidebar_sign").ready(function(){
+    if(isSearch == 1){
+        url = url + recipe_id;
+        $.ajax({
+            type: "GET",
+            url: url
+        }).success(function(msg){
+            var dishList = msg.result.list;
+            //dish1
+            $("#d1image").attr("src",dishList[0].pic);
+            $("#d1name").text(dishList[0].name);
+            $("#d1tag").text(dishList[0].tag);
+            $("#d1content").html(dishList[0].content);
+            $("#d1id").text(dishList[0].id);
+            //dish2
+            $("#d2image").attr("src",dishList[1].pic);
+            $("#d2name").text(dishList[1].name);
+            $("#d2tag").text(dishList[1].tag);
+            $("#d2content").html(dishList[1].content);
+            $("#d2id").text(dishList[1].id);
+            //dish3
+            $("#d3image").attr("src",dishList[2].pic);
+            $("#d3name").text(dishList[2].name);
+            $("#d3tag").text(dishList[2].tag);
+            $("#d3content").html(dishList[2].content);
+            $("#d3id").text(dishList[2].id);
+            //dish4
+            $("#d4image").attr("src",dishList[3].pic);
+            $("#d4name").text(dishList[3].name);
+            $("#d4tag").text(dishList[3].tag);
+            $("#d4content").html(dishList[3].content);
+            $("#d4id").text(dishList[3].id);
+            //dish5
+            $("#d5image").attr("src",dishList[4].pic);
+            $("#d5name").text(dishList[4].name);
+            $("#d5tag").text(dishList[4].tag);
+            $("#d5content").html(dishList[4].content);
+            $("#d5id").text(dishList[4].id);
+
+            $("#isSearch b").text("您搜索的是：");
+            $("#keyword").text(recipe_name);
+
+        }).fail(function(msg){
+            console.error("error!")
+        })
+    }
+    else{
+        url = url + 2;
+        $.ajax({
+            type: "GET",
+            url: url
+        }).success(function(msg){
+            var dishList = msg.result.list;
+            //dish1
+            $("#d1image").attr("src",dishList[0].pic);
+            $("#d1name").text(dishList[0].name);
+            $("#d1tag").text(dishList[0].tag);
+            $("#d1content").html(dishList[0].content);
+            $("#d1id").text(dishList[0].id);
+            //dish2
+            $("#d2image").attr("src",dishList[1].pic);
+            $("#d2name").text(dishList[1].name);
+            $("#d2tag").text(dishList[1].tag);
+            $("#d2content").html(dishList[1].content);
+            $("#d2id").text(dishList[1].id);
+            //dish3
+            $("#d3image").attr("src",dishList[2].pic);
+            $("#d3name").text(dishList[2].name);
+            $("#d3tag").text(dishList[2].tag);
+            $("#d3content").html(dishList[2].content);
+            $("#d3id").text(dishList[2].id);
+            //dish4
+            $("#d4image").attr("src",dishList[3].pic);
+            $("#d4name").text(dishList[3].name);
+            $("#d4tag").text(dishList[3].tag);
+            $("#d4content").html(dishList[3].content);
+            $("#d4id").text(dishList[3].id);
+            //dish5
+            $("#d5image").attr("src",dishList[4].pic);
+            $("#d5name").text(dishList[4].name);
+            $("#d5tag").text(dishList[4].tag);
+            $("#d5content").html(dishList[4].content);
+            $("#d5id").text(dishList[4].id);
+
+        }).fail(function(msg){
+            console.error("error!")
+        })
+    }
+    
 })
 
 $("#changeRecipe2").click(function(){
